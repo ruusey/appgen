21e3c2c33bf6f9a4c3cb22f4501d378cb04e2e7f
Robert Usey <ruusey@gmail.com>
2020-07-14 14:17:10 -0400

    Added VUE frontent

--- src/main/java/com/appgen/rest/RestService.java
+++ src/main/java/com/appgen/rest/RestService.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
 import com.appgen.models.DatabaseEntity;
 import com.fasterxml.jackson.core.JsonProcessingException;
